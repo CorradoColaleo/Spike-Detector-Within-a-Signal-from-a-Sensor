afa8c4652c5d3678370f43c41f4549aee98af8f4
CorradoColaleo <ingegnerecolaleo@gmail.com>
2025-03-02 21:51:52 +0100

    commit detector backend

--- 3-detector/backend/src/top.sv
+++ 3-detector/backend/src/top.sv
@@ -4,6 +4,12 @@ module top (input logic clk, rst,
             input logic signed [10:0] x,
             output logic spike);
 
+logic signed [10:0] output_iir;
+logic signed [11:0] output_enfasi;
+
+iir blocco1 (.clk(clk),.rst(rst),.x(x),.z(output_iir));
+enfasi blocco2(.clk(clk),.rst(rst),.z(output_iir),.q(output_enfasi));
+detector blocco3(.clk(clk),.rst(rst),.enable(enable),.din(din),.q(output_enfasi),.spike(spike));
 
 endmodule
 
